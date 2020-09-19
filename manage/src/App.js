7363a4e26d273578872d1438f529f452b4eddb89
guai-tongxue <1119362740@qq.com>
2020-09-19 23:46:18 +0800

    环境集成

--- manage/src/App.js
+++ manage/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.scss';
 
-// 引入高阶组件
-import { withRouter } from 'react-router-dom'
-
-class App extends React.Component {
-  state = {
-    num : 10
-  }
-  changeNumber = ()=>{
-    this.setState({
-      num : this.state.num + 1
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        HELLO testsss
-        <button onClick={this.changeNumber}>change Number</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      Hellow sspai
+    </div>
+  );
 }
 
 export default App;
--- manage/src/index.js
+++ manage/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import {HashRouter,BrowserRouter} from 'react-router-dom'
 
-const Router = process.evn.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
+const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
+</Router>,
+document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
